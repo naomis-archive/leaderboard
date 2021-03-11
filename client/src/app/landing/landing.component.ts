3701195cda4d9d3b6042d6b6b370fd7cff895a35
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-10 16:10:37 -0800

    feat: initial landing page
    Build out the initial landing page, rendering the data from the
    API call. Will test a deployment from this step.

--- client/src/app/landing/landing.component.ts
+++ client/src/app/landing/landing.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalDataInt } from 'src/interfaces/GlobalDataInt';
+import { GetDataService } from '../get-data.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent implements OnInit {
-
-  constructor() { }
+  public data: any;
+  public loaded: boolean = false;
+  public updated: any;
+  public crowdin: any;
+  public forum: any;
+  public github: any;
+  public news: any;
+  constructor(private GetDataService: GetDataService) {}
 
   ngOnInit(): void {
+    this.GetDataService.getData().subscribe((data) => {
+      this.data = data;
+      this.updated = new Date(data.updated_on).toLocaleDateString() + " " + new Date(data.updated_on).toLocaleTimeString();
+      this.crowdin = data.crowdin.slice(0, 10);
+      console.log(this.crowdin)
+      this.forum = data.forum.slice(0, 10);
+      this.github = data.github.slice(0, 10);
+      this.news = data.news.slice(0, 10);
+      this.loaded = true;
+    });
   }
-
 }
