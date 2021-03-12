8f1964ec2dd14baf5d146cf104c03cc295522e28
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-12 13:38:54 -0800

    feat: implement nav bar
    Creates a nav bar skeleton to allow for the expansion to multiple
    pages.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrowdinComponent } from './crowdin/crowdin.component';
 import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: LandingComponent },
+  { path: 'crowdin', component: CrowdinComponent },
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GetDataService } from './get-data.service';
+import { CrowdinComponent } from './crowdin/crowdin.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingComponent],
+  declarations: [AppComponent, LandingComponent, CrowdinComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [GetDataService],
   bootstrap: [AppComponent],
