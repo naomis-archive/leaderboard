12670fb5db9a3add0b3b1f23237a932738e92af5
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-16 16:11:57 -0700

    feat(client): test AppComponent
    Add tests for the AppComponent.

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,26 +1,51 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavComponent } from './nav/nav.component';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let compiled: any;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent, NavComponent, FooterComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app).toBeTruthy('app does not build');
   });
 
-  it(`should have as title 'client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
+  it(`should have as title 'freeCodeCamp Leaderboard'`, () => {
+    expect(app.title).toEqual(
+      'freeCodeCamp Leaderboard',
+      'did not have correct title'
+    );
+  });
+
+  it('should render the nav bar', () => {
+    expect(compiled.querySelector('app-nav')).toBeTruthy(
+      'does not render nav bar'
+    );
+  });
+
+  it('should render the router component', () => {
+    expect(compiled.querySelector('router-outlet')).toBeTruthy(
+      'does not render the router outlet'
+    );
+  });
+
+  it('should render the footer component', () => {
+    expect(compiled.querySelector('app-footer')).toBeTruthy(
+      'does not render the app footer'
+    );
   });
 });
