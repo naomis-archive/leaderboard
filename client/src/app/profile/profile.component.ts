97cf1c7ce3aa02e5678f4353481b92b3dca0b69f
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-19 15:11:15 -0700

    feat(client): get data for form
    Adds the logic to load the API data for the ProfileComponent.

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GetDataService } from '../get-data.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  public data: any;
+  public loaded = false;
+  public updated: any;
+  public crowdin: any;
+  public forum: any;
+  public github: any;
+  public news: any;
+  constructor(private getDataService: GetDataService) {}
 
   ngOnInit(): void {
+    this.getDataService.getData().subscribe((data) => {
+      this.data = data;
+      this.updated =
+        new Date(data.updated_on).toLocaleDateString() +
+        ' ' +
+        new Date(data.updated_on).toLocaleTimeString();
+      this.crowdin = data.crowdin.slice(0, 10);
+      this.forum = data.forum.slice(0, 10);
+      this.github = data.github.slice(0, 10);
+      this.news = data.news.slice(0, 10);
+      this.loaded = true;
+    });
   }
-
 }
