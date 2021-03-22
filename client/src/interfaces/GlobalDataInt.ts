555e9f72cb41766c714c3394c2d53f9ab34f94cb
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-22 06:22:09 -0700

    feat(client): expand interfaces
    Expand the interface definitions for the API data.

--- client/src/interfaces/GlobalDataInt.ts
+++ client/src/interfaces/GlobalDataInt.ts
@@ -1,32 +1,39 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+export interface CrowdinDataInt {
+  name: string;
+  languages: string[];
+  translations: number;
+  avatar: string;
+}
+
+export interface ForumDataInt {
+  username: string;
+  name: string;
+  likes: number;
+  liked: number;
+  avatar: string;
+  url: string;
+}
+
+export interface GithubDataInt {
+  username: string;
+  name: string;
+  commits: number;
+  avatar: string;
+  url: string;
+}
+
+export interface NewsDataInt {
+  name: string;
+  username: string;
+  url: string;
+  avatar: string;
+  posts: number;
+}
 export interface GlobalDataInt {
-  crowdin: {
-    name: string;
-    languages: string[];
-    translations: number;
-    avatar: string;
-  }[];
-  forum: {
-    username: string;
-    name: string;
-    likes: number;
-    liked: number;
-    avatar: string;
-    url: string;
-  }[];
-  github: {
-    username: string;
-    name: string;
-    commits: number;
-    avatar: string;
-    url: string;
-  }[];
-  news: {
-    name: string;
-    username: string;
-    url: string;
-    avatar: string;
-    posts: number;
-  }[];
+  crowdin: CrowdinDataInt[];
+  forum: ForumDataInt[];
+  github: GithubDataInt[];
+  news: NewsDataInt[];
   updated_on: Date;
 }
