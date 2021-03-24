2541f602dc4743c028f0d49c3ab9845b88a7a14f
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-24 11:23:30 -0700

    feat(api): use logger in errorHandler
    Use the custom logger instead of console.log in the error handler.

--- api/src/utils/errorHandler.ts
+++ api/src/utils/errorHandler.ts
@@ -1,6 +1,7 @@
 import Sentry from "@sentry/node";
+import { logHandler } from "./logHandler";
 export const errorHandler = (context: string, error: unknown): void => {
-  console.error(`There was an error in the ${context}:`);
-  console.error(error);
+  logHandler.log("error", `There was an error in the ${context}:`);
+  logHandler.log("error", error);
   Sentry.captureException(error);
 };
