5904cc827b8dcbf58b7bc037c42df429d9604cf8
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-24 17:12:55 -0700

    feat(client): update leaderboard
    Use the new endpoint to pull user data from the database, then
    generate aggregate scores within the client for that data.

--- client/src/interfaces/ProfileInt.ts
+++ client/src/interfaces/ProfileInt.ts
@@ -6,3 +6,17 @@ export interface UserFormInt {
   github: string;
   news: string;
 }
+
+export interface UserDataInt {
+  username: string;
+  crowdin: string;
+  forum: string;
+  github: string;
+  news: string;
+}
+
+export interface ParsedUserInt {
+  username: string;
+  aggregate: number;
+  avatar: string;
+}
