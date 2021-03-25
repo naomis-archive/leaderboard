278140d2ccddb1a119c3d5409e6e00a2ec1f7e62
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-25 09:24:37 -0700

    feat(client): leaderboard tests
    Update tests for the top-ten component to reflect the new content.

---
+++ client/src/data/testUserData.ts
@@ -0,0 +1,19 @@
+import { ParsedUserInt } from 'src/interfaces/ProfileInt';
+
+export const testUserData: ParsedUserInt[] = [
+  {
+    username: 'nhcarrigan',
+    avatar: 'https://www.nhcarrigan.com',
+    aggregate: 5,
+  },
+  {
+    username: 'nhcarrigan but best',
+    avatar: 'https://best.com',
+    aggregate: 10,
+  },
+  {
+    username: 'nhcarrigan but better',
+    avatar: 'https://better.com',
+    aggregate: 7,
+  },
+];
